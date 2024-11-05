ffecc797ec8ca802efb7186ee10bf9163a2d0e88
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-05 06:36:46 +0000

    remove oldmain

--- src/editor/main.c
+++ src/editor/main.c
@@ -1,17 +1,5 @@
 #include "editor.h"
 
-int oldmain(int argc , char* argv[]){
-
-  pe_renderer_type = PEWMOPENGLES2;
-  command_parse_from_command_line(argc, argv);
-
-  pe_editor_init();
-
-  pe_program_main_loop(&editor_main_loop, window_editor_main);
-
-  editor_finish();
-
-}
 
 void aloop(){
 
