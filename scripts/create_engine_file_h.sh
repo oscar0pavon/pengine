24d423e1f965feae49a0082b6671cddf5cb4da06
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-09 06:36:28 +0000

    improved shaders compilation

--- scripts/create_engine_file_h.sh
+++ scripts/create_engine_file_h.sh
@@ -10,14 +10,14 @@ echo "#ifndef _FILES_H_" > ${FILE}
 echo "#define _FILES_H_" >> ${FILE}
 
 
-echo "#define file_std_vert \"${WORKDIR}/src/shaders/std.vert\"" >> ${FILE}
-echo "#define file_diffuse_frag \"${WORKDIR}/src/shaders/diffuse.frag\"" >> ${FILE}
+echo "#define file_std_vert \"${WORKDIR}/src/shaders/std_vert.glsl\"" >> ${FILE}
+echo "#define file_diffuse_frag \"${WORKDIR}/src/shaders/diffuse_frag.glsl\"" >> ${FILE}
 
 
 echo "#define file_skin_vertex_shader \"${WORKDIR}/src/shaders/skin_vertex_shader.glsl\"" >> ${FILE}
 
 
-echo "#define file_vertex_modeling \"${WORKDIR}/src/shaders/vertex_modelling.frag\"" >> ${FILE}
+echo "#define file_vertex_modeling \"${WORKDIR}/src/shaders/vertex_modelling.glsl\"" >> ${FILE}
 
 #editor gizmos
 echo "#define file_translate_glb \"${WORKDIR}/content/editor/translate.glb\"" >> ${FILE}
@@ -40,14 +40,14 @@ echo "#define file_other_vert_spv \"${WORKDIR}/src/shaders/other_vert.spv\"" >> 
 echo "#define file_blue_frag_spv \"${WORKDIR}/src/shaders/blue_frag.spv\"" >> ${FILE}
 echo "#define file_vert_spv \"${WORKDIR}/src/shaders/vert.spv\"" >> ${FILE}
 echo "#define file_frag_spv \"${WORKDIR}/src/shaders/frag.spv\"" >> ${FILE}
-echo "#define file_in_position_spv \"${WORKDIR}/src/shaders/in_position.spv\"" >> ${FILE}
+echo "#define file_in_position_spv \"${WORKDIR}/src/shaders/in_position_vert.spv\"" >> ${FILE}
 echo "#define file_diffuse_vert_spv \"${WORKDIR}/src/shaders/diffuse_vert.spv\"" >> ${FILE}
 echo "#define file_diffuse_frag_spv \"${WORKDIR}/src/shaders/diffuse_frag.spv\"" >> ${FILE}
 echo "#define file_skinned_spv \"${WORKDIR}/src/shaders/skinned.spv\"" >> ${FILE}
 
 #font
 
-echo "#define file_font\"${WORKDIR}/content/DejaVuSerif.ttf\"" >> ${FILE}
+echo "#define file_font \"${WORKDIR}/content/DejaVuSerif.ttf\"" >> ${FILE}
 
 #chess
 
